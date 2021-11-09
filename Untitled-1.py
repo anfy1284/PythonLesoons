8ccfebe141a35a8be54d7aee8e159bd2d75955ee
anfy <anfy1284@gmail.com>
2021-11-09 22:24:58 +0300

    commit1

---
+++ Untitled-1.py
@@ -0,0 +1,18 @@
+v = 10 ** 4
+print(v)
+#name = input("youre name")
+#print("youre name is ", name)
+
+
+answer = input("Let's do ? (Y/N) ")
+if answer == 'Y':
+
+    print("Let's go!")
+    pass
+    print("Let's go! 2")
+
+elif answer == 'N':
+    print("Why not?")
+else:
+    print("good by!")
+
